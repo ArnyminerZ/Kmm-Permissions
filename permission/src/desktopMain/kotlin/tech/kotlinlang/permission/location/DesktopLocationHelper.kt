ea4c2faa8ef285f19fcc6065ffa2ad1503b64996
Arnau Mora <arnyminerz@proton.me>
2025-10-28 17:27:48 +0100

    Handle all exceptions

--- permission/src/desktopMain/kotlin/tech/kotlinlang/permission/location/DesktopLocationHelper.kt
+++ permission/src/desktopMain/kotlin/tech/kotlinlang/permission/location/DesktopLocationHelper.kt
@@ -1,12 +1,10 @@
 package tech.kotlinlang.permission.location
 
-import java.io.IOException
-
 class DesktopIPLocationHelper(private val provider: IPLocationProvider) : LocationHelper {
     override suspend fun fetchLastKnownLocation(): LocationRequestResult {
         return try {
             provider.fetchIPLocation()
-        } catch (_: IOException) {
+        } catch (_: Exception) {
             LocationRequestResult.NoLastLocationFound
         }
     }
